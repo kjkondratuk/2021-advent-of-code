445e217336824aefeb4d6e2adb0a09944bd4cd87
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2021-12-05 23:00:01 -0600

    add run configs and working day-4 part 1

--- day-4/main.go
+++ day-4/main.go
@@ -3,11 +3,52 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	bytes, _ := ioutil.ReadFile("inputs/day-4-small.txt")
+	turns, cards := parseFile("inputs/day-4.txt")
+	fmt.Printf("Cards: %d\n", len(cards))
+	fmt.Printf("Turns: %+v\n", turns)
+
+	turn, winner := determineWinner(turns, cards)
+	fmt.Printf("Turn won: %d\n", turn)
+	fmt.Printf("Winner index: %d\n", winner)
+
+	fmt.Printf("Index: %d - Card: %+v\n", winner, cards[winner].GetSpaces())
+	winNum, _ := strconv.Atoi(turns[turn - 1])
+	score := cards[winner].Score(winNum)
+	fmt.Printf("Score: %d", score)
+}
+
+func determineWinner(turns []string, cards map[int]Card) (int, int) {
+	firstWinner := -1
+	var turnsTaken []string
+	for i := 0; i < len(turns); i++ {
+		turnsTaken = append(turnsTaken, turns[i])
+		for ci, c := range cards {
+			if filled := c.Mark(turns[i]); filled {
+				if firstWinner < 0 {
+					//fmt.Printf("filled card: %d on turn %d\n", ci, i)
+					//fmt.Printf("turns: %+v\n", turnsTaken)
+					//fmt.Println("Card:")
+					//for _, row := range c.GetSpaces() {
+					//	fmt.Printf("%+v\n", row)
+					//}
+					firstWinner = ci
+					goto winnerEnd
+				}
+			}
+		}
+	}
+
+	winnerEnd:
+	return len(turnsTaken), firstWinner
+}
+
+func parseFile(file string) ([]string, map[int]Card) {
+	bytes, _ := ioutil.ReadFile(file)
 
 	lines := strings.Split(string(bytes), "\n")
 
@@ -36,26 +77,7 @@ func main() {
 		nextCard++
 	}
 
-	fmt.Printf("Cards: %d\n", len(cards))
-
-	firstWinner := -1
-	var turnsTaken []string
-	for i := 0; i < len(turns); i++ {
-		turnsTaken = append(turnsTaken, turns[i])
-		for ci, c := range cards {
-			if filled := c.Mark(turns[i]); filled {
-				if firstWinner < 0 {
-					fmt.Printf("filled card: %d on turn %d\n", ci, i)
-					fmt.Printf("turns: %+v\n", turnsTaken)
-					fmt.Println("Card:")
-					for _, row := range c.GetSpaces() {
-						fmt.Printf("%+v\n", row)
-					}
-					firstWinner = ci
-				}
-			}
-		}
-	}
+	return turns, cards
 }
 
 type card struct {
@@ -67,6 +89,7 @@ type Card interface {
 	AppendRow(s []string)
 	Mark(s string) bool
 	GetSpaces() [][]string
+	Score(turn int) int
 }
 
 func NewCard() Card {
@@ -88,24 +111,37 @@ func (c *card) AppendRow(s []string) {
 }
 
 func (c *card) GetSpaces() [][]string {
-	return c.spaces
+	colorizedSpaces := c.spaces
+
+	for ri, row := range colorizedSpaces {
+		for ci, v := range row {
+			if c.marks[ri][ci] {
+				colorizedSpaces[ri][ci] = "\033[31m" + v + "\033[0m"
+			}
+		}
+	}
+
+	return colorizedSpaces
 }
 
 func (c *card) Mark(s string) bool {
-	for y, row := range c.spaces {
-		for x, col := range row {
+	for ri, row := range c.spaces {
+		for ci, col := range row {
 			if col == s {
-				c.marks[y][x] = true
-
-				completed := checkRow(c.marks[y])
+				c.marks[ri][ci] = true
+				completed := checkRow(c.marks[ri])
 				if completed {
+					fmt.Printf("completed based on row\n")
 					return completed
 				} else {
 					b := true
 					for i := 0; i < 5; i++ {
-						if c.marks[y][i] {
+						if c.marks[ri][i] {
 							b = false
 						}
+					}
+					if b {
+						fmt.Printf("columns checked\n")
 					}
 					return b
 				}
@@ -113,6 +149,24 @@ func (c *card) Mark(s string) bool {
 		}
 	}
 	return false
+}
+
+func (c *card) Score(winnerNum int) int {
+	total := 0
+	for ri, row := range c.marks {
+		for ci, marked := range row {
+			if !marked {
+				num, _ := strconv.Atoi(c.spaces[ri][ci])
+				//fmt.Printf("adding: %d\n", num)
+				total += num
+			} else {
+				//fmt.Printf("not marked\n")
+			}
+		}
+	}
+	fmt.Printf("Total: %d\n", total)
+	fmt.Printf("Turn: %d\n", winnerNum)
+	return total * winnerNum
 }
 
 func checkRow(s []bool) bool {
