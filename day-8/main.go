6dd093e95b9f0aa703f0affafa3c5d5d72a6b2f4
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2021-12-08 23:10:55 -0600

    start adding data reader

--- day-8/main.go
+++ day-8/main.go
@@ -1,5 +1,10 @@
 package main
 
+import "github.com/kjkondratuk/2021-advent-of-code/lib"
+
 func main() {
+	data := lib.NewDataReader("inputs/day-8-small.txt").Read()
+	lines := data.([]string)
+
 	return
 }
